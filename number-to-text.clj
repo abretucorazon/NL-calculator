b81204cc3db2d4078bc84c2f84b97792e00969a2
Marvin Nguyen <cafemarvin@146verdi.com>
2019-03-19 07:05:08 -0400

    Update number-to-text.clj
    New approach - split number into chunks of 3 digits before converting to text - untested

--- number-to-text.clj
+++ number-to-text.clj
@@ -21,7 +21,7 @@
 (def tees2090 (zipmap (range 20 100 10) tees))
 
 ; Map of text to units of hundreds, thousands, millions
-(def units ["hundred" "thousand" "million" ])
+(def units ["hundred" "thousand" "million"])
 (def units-digits [100 1000 1000000])
 (def units-map (zipmap units-digits units))
 
@@ -29,19 +29,42 @@
 (def num->word (conj tens09 teens1019 tees2090 units-map))
 
 ; Forward declarations
-(declare num->text+unit three-digit->text two-digit->text)
+(declare num->text+unit three-digit->text two-digit->text n->3digits)
 
 ; Convert a positive number between 0 and 999 millions into its textual form
 ; usage: (num-text 123) -> "one hundred twenty three"
 (defn number->text [n]
     (s/trim
         (let [millions (/ n one-million) 
-                thousands (/ (- n millions) one-thousand)) 
+                thousands (/ (- n m˜illions) one-thousand)) 
                 hundreds  (mod n one-thousand)]
                 (s/str  (num->text+unit    millions  unit-million  three-digit->text)  
                         (num->text+unit    thousands unit-thousand three-digit->text)  
                         (three-digit->text hundreds))))
 
+(def no-text "")
+(def ordered-units [(num->word unit-million) (num->word unit-thousand) no-text])
+
+; Ranges for validating input
+(def valid-min 0)
+(def valid-max 999999999)
+(def bad-input-msg (s/str "Number must be between " valid-min " and " valid-max))
+
+(defn n->t [n]
+    (assert (and (>= n 0) (<= n valid-max)) bad-input-msg)
+    (let [lst-3digits (n->3digits n) 
+          vec-units (subvec ordered-units (- (count ordered-units) (count lst-3digits)))]
+          (map #((if (> %1 0) 
+                    (s/str (three-digit->text %1) " " %2 " ") 
+                    no-text)) 
+                lst-3digits vec-units)))
+
+; Split a number into a list of 3-digit 
+(defn n->3digits [n]
+    (loop [x n res '()]
+        (if (= x  0)
+            res 
+            (recur (quot x 1000) (conj res (mod x 1000))))))
 
 ;; Format a positive number into text and combined with its unit in text form
 ;; e.g. (num->text+unit 123 1000 three-digit->text) -> one hundred twenty three thousand 
